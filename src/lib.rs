972823d08c664719eea5cf624b369c30ce4ff490
Jack Kelly <jack@openclimatefix.org>
2024-12-06 12:54:14 +0000

    Using Arc<dyn ObjectStore>

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,58 @@
+use std::sync::Arc;
+
 use object_store::{path::Path, ListResult, ObjectStore};
 
-pub trait ListWithDepth: ObjectStore {
-    /// List objects with the given prefix and depth, and an implementation specific delimiter.
-    /// Returns common prefixes (directories) in addition to object metadata.
-    ///
-    /// For example, say that a bucket contains the following objects:
-    /// - `a.txt`
-    /// - `foo/b.txt`
-    /// - `foo/bar/c.txt`
-    /// - `foo/bar/d.txt`
-    ///
-    /// Calling `list_with_depth` with `prefix = None` and `depth = 0` is equivalent to calling
-    /// `ObjectStore::list_with_delimiter(None)`. It will return the objects and common prefixes at
-    /// the root: `a.txt` and `foo/`.
-    ///
-    /// Calling `list_with_depth` with `depth = 1` will recurse once, and return `b.txt` and
-    /// `foo/bar/`.
-    ///
-    /// Prefixes are evaluated on a path segment basis, i.e. `foo/bar` is a prefix of `foo/bar/x`
-    /// but not of `foo/bar_baz/x`.
-    async fn list_with_depth(
-        &self,
-        prefix: Option<&Path>,
-        depth: usize,
-    ) -> object_store::Result<ListResult> {
-        if depth == 0 {
-            return self.list_with_delimiter(prefix).await;
-        }
-        Ok(ListResult {
-            objects: vec![],
-            common_prefixes: vec![],
-        })
+/// List objects with the given prefix and depth, and an implementation specific delimiter.
+/// Returns common prefixes (directories) in addition to object metadata.
+///
+/// For example, say that a bucket contains the following objects:
+/// - `a.txt`
+/// - `foo/b.txt`
+/// - `foo/bar/c.txt`
+/// - `foo/bar/d.txt`
+///
+/// Calling `list_with_depth` with `prefix = None` and `depth = 0` is equivalent to calling
+/// `ObjectStore::list_with_delimiter(None)`. It will return the objects and common prefixes at
+/// the root: `a.txt` and `foo/`.
+///
+/// Calling `list_with_depth` with `depth = 1` will recurse once, and return `b.txt` and
+/// `foo/bar/`.
+///
+/// Prefixes are evaluated on a path segment basis, i.e. `foo/bar` is a prefix of `foo/bar/x`
+/// but not of `foo/bar_baz/x`.
+pub async fn list_with_depth(
+    store: Arc<dyn ObjectStore>,
+    prefix: Option<&Path>,
+    depth: usize,
+) -> object_store::Result<ListResult> {
+    let list_result = store.list_with_delimiter(prefix).await?;
+    if depth == 0 {
+        return Ok(list_result);
     }
+
+    for _d in 0..depth {
+        let mut common_prefixes_at_this_depth = vec![];
+        for common_prefix in list_result.common_prefixes.clone() {
+            common_prefixes_at_this_depth.push(tokio::spawn(list_with_delimiter_take_ownership(
+                store.clone(),
+                common_prefix,
+            )));
+        }
+    }
+
+    Ok(ListResult {
+        objects: vec![],
+        common_prefixes: vec![],
+    })
 }
 
-impl<T: ObjectStore> ListWithDepth for T {}
+// Helper function
+async fn list_with_delimiter_take_ownership(
+    store: Arc<dyn ObjectStore>,
+    prefix: Path,
+) -> object_store::Result<ListResult> {
+    store.list_with_delimiter(Some(&prefix)).await
+}
 
 #[cfg(test)]
 mod tests {
@@ -52,12 +70,13 @@ mod tests {
     }
 
     /// Returns (object_paths, common_prefixes).
-    async fn test_list_with_depth_n(depth: usize) -> object_store::Result<(Vec<Path>, Vec<Path>)> {
+    async fn test_depth_n(depth: usize) -> object_store::Result<(Vec<Path>, Vec<Path>)> {
         let store = create_in_memory_store().await?;
+        let store = Arc::new(store);
         let ListResult {
             objects,
             common_prefixes,
-        } = store.list_with_depth(None, depth).await?;
+        } = list_with_depth(store, None, depth).await?;
         let object_paths = objects
             .into_iter()
             .map(|object_meta| object_meta.location)
@@ -66,8 +85,8 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_list_with_depth_0() -> object_store::Result<()> {
-        let (object_paths, common_prefixes) = test_list_with_depth_n(0).await?;
+    async fn test_depth_0() -> object_store::Result<()> {
+        let (object_paths, common_prefixes) = test_depth_n(0).await?;
         assert_eq!(object_paths.len(), 1);
         assert_eq!(object_paths[0], Path::from("a.txt"));
         assert_eq!(common_prefixes, vec![Path::from("foo")]);
@@ -75,8 +94,8 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_list_with_depth_1() -> object_store::Result<()> {
-        let (object_paths, common_prefixes) = test_list_with_depth_n(1).await?;
+    async fn test_depth_1() -> object_store::Result<()> {
+        let (object_paths, common_prefixes) = test_depth_n(1).await?;
         assert_eq!(object_paths.len(), 1);
         assert_eq!(object_paths[0], Path::from("b.txt"));
         assert_eq!(common_prefixes, vec![Path::from("foo/bar")]);
